8fec4ed53cfe4aea94e660204184ea9629a2c2a9
WDD 230 - Leticia Leal Santos <leticialeal@byui.edu>
2020-04-25 16:40:29 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var date = new Date();
+
+document.getElementById("year").textContent = date.getFullYear();
+
+var lastModified = document.lastModified;
+
+document.getElementById("updated").textContent = lastModified;
